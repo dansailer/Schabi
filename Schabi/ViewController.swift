91e8d6e64de3f76c07099bcfffb074a514279591
Daniel Sailer <daniel@sailer.com.au>
2022-03-25 07:53:45 +0100

    Initial version with settings page

--- Schabi/ViewController.swift
+++ Schabi/ViewController.swift
@@ -6,14 +6,61 @@
 //
 
 import UIKit
+import WebKit
+//import KeychainSwift
 
 class ViewController: UIViewController {
 
+    let webView: WKWebView = {
+        let prefs = WKWebpagePreferences()
+        prefs.allowsContentJavaScript = true
+        let configuration = WKWebViewConfiguration()
+        configuration.defaultWebpagePreferences = prefs
+        configuration.limitsNavigationsToAppBoundDomains = true
+        let webView = WKWebView(frame: .zero,
+                                configuration: configuration)
+        return webView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(webView)
+        //let keychain = KeychainSwift()
+        //let username = (keychain.get("username") ?? "")
+        //let password = (keychain.get("password") ?? "")
+        //keychain.set("", forKey: "username", withAccess: .accessibleWhenUnlocked)
+        //keychain.set("", forKey: "password", withAccess: .accessibleWhenUnlocked)
+        
+        let username : String? = UserDefaults.standard.string(forKey: "username")
+        let password : String? = UserDefaults.standard.string(forKey: "password")
+        if (username == nil || password == nil || username == "" || password == "") {
+            if let url = URL(string:UIApplication.openSettingsURLString) {
+                if UIApplication.shared.canOpenURL(url) {
+                    UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                    DispatchQueue.main.asyncAfter(deadline: .now()) {
+                        exit(0)
+                    }
+                }
+            }
+        }
+        
+        guard let url = URL(string: "https://www.schabi.ch/Account/Login") else {
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        let postString = "UserName=" + username! + "&Password=" + password! + "&RememberMe=true&returnUrl=";
+        //NSLog("postData: \(postString)")
+        request.httpBody = postString.data(using: String.Encoding.utf8);
+        request.addValue("application/x-www-form-urlencoded", forHTTPHeaderField: "content-type")
+        webView.load(request)
     }
 
-
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        webView.frame = view.bounds
+        
+    }
 }
 
